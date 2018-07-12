ec225be4988cba63efedacf6ec4d3eff03465f23
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-12 18:07:13 -0400

    Add base query for all signals

--- app/graphql/kacha_api_schema.rb
+++ app/graphql/kacha_api_schema.rb
@@ -1,4 +1,4 @@
 class KachaApiSchema < GraphQL::Schema
-  mutation(Types::MutationType)
+  # mutation(Types::MutationType)
   query(Types::QueryType)
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,4 +1,12 @@
 class Types::QueryType < Types::BaseObject
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
+
+  field :active_signals, [Types::PlaySignalType], null: true do
+    description 'Get all active signals'
+  end
+
+  def active_signals
+    PlaySignal.all_active
+  end
 end
