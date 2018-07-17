fad7012def3390ba094f969f0180bf90e8f2d1f8
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-16 20:50:08 -0400

    Seed dummy User instances

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'graphql'
 gem 'graphiql-rails'
 gem 'jwt'
 gem 'rack-cors'
+gem 'faker'
 
 group :development, :test do
   gem 'dotenv-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+  provider = ['twitter', 'discord'].sample
+  fake_user = Faker::Twitter.unique.user
+  provider_username = fake_user[:screen_name]
+  provider_username += '#123' if provider == 'discord'
+
+  user = User.find_or_create_by(
+    username: fake_user[:name],
+    avatar_platform: provider,
+  )
+  user.identities.create(
+    uid: fake_user[:id_str],
+    provider: provider,
+    username: fake_user[:screen_name],
+    image_url: fake_user[:profile_image_url],
+  )
+  user.play_signal.update(
+    message: fake_user[:description],
+    lat: Faker::Address.latitude,
+    lng: Faker::Address.longitude,
+    published: true,
+    end_time: DateTime.current + 50.years,
+  )
+
+  p "Created #{user.username}"
+end
