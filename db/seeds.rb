a7a03089baf88a4aca349325ee8187f4bacbe140
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-18 09:13:54 -0400

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,33 @@
-50.times do
-  provider = ['twitter', 'discord'].sample
+User.destroy_all
+
+100.times do
   fake_user = Faker::Twitter.unique.user
-  provider_username = fake_user[:screen_name]
-  provider_username += '#123' if provider == 'discord'
+  provider = ['twitter', 'discord'].sample
+  providers = [true, false].sample ? ['twitter', 'discord'] : [provider]
 
   user = User.find_or_create_by(
     username: fake_user[:name],
-    avatar_platform: provider,
+    avatar_platform: providers.sample,
   )
-  user.identities.create(
-    uid: fake_user[:id_str],
-    provider: provider,
-    username: fake_user[:screen_name],
-    image_url: fake_user[:profile_image_url],
-  )
+
+  # Randomly generate just one or both identities.
+  providers.each do |p|
+    username = fake_user[:screen_name]
+    username += '#123' if p === 'discord'
+    user.identities.create(
+      uid: fake_user[:id_str],
+      provider: p,
+      username: username,
+      image_url: fake_user[:profile_image_url],
+    )
+  end
+
   user.play_signal.update(
     message: fake_user[:description],
     lat: Faker::Address.latitude,
     lng: Faker::Address.longitude,
     published: true,
-    end_time: DateTime.current + 50.years,
+    end_time: rand(Time.now..3.hours.from_now),
   )
 
   p "Created #{user.username}"
