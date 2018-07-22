67383b324b4e074901a6d386b3d13acdeaa37b7d
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-22 15:38:30 -0400

    Allow for updating signal lat/lng

--- app/graphql/mutations/update_signal.rb
+++ app/graphql/mutations/update_signal.rb
@@ -1,16 +1,20 @@
 class Mutations::UpdateSignal < Mutations::BaseMutation
   argument :message, String, required: true
   argument :end_time, String, required: true
+  argument :lat, Float, required: true
+  argument :lng, Float, required: true
   argument :published, Boolean, required: true
 
   field :errors, [String], null: false
 
-  def resolve(message:, end_time:, published:)
+  def resolve(message:, end_time:, lat:, lng:, published:)
     return unauthorized_hash if context[:current_user].nil?
 
     signal = context[:current_user].play_signal
     signal.message = message
     signal.end_time = end_time
+    signal.lat = lat
+    signal.lng = lng
     signal.published = published
 
     if signal.save
